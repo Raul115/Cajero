2bc452e9e51bfb73ce5d6839346267224a6f6f48
Rulo <Rulo@Raul.Home>
2019-03-01 01:02:23 +0100

    Comprueba clientes y registra

--- src/Banco/Cajero.java
+++ src/Banco/Cajero.java
@@ -37,66 +37,89 @@ public class Cajero {
 
     public void inicio() throws IOException {
         String dni;
-        int pin;
+        int pin, opcion;
         boolean existe, existeCuenta;
+        System.out.println("Bienvenido.");
         dni = DNI.pedirDNI();
-        pin = PIN.pedirPIN();
         existe = clienteExiste(dni);
-        while (existe && pin != -1) {
-            existeCuenta = cuentaExiste(dni);
-            if (!existeCuenta) {
-                crearCuenta(dni);
-            }
-
-            menu();
-        }
         if (!existe) {
-            System.out.println("Registrando cliente.");
-            registrarCliente(dni, pin);
-        }
-    }
-
-    public void menu() throws IOException {
-        int opcion = Number.askForNumberInt("Selecciona las siguientes opciones: "
-                + "<0 salir> <1 ingresar> <2 retirar> <3 movimientos>: ", 0, 3);
-        while (opcion != 0) {
+            opcion = Number.askForNumberInt("No está registrado. ¿Desea registrarse como cliente? <1 si> <2 no>: ", 1, 2);
             switch (opcion) {
                 case 1:
-                    ingresar();
+                    registrarCliente(dni);
+                    menu();
                     break;
                 case 2:
-                    retirar();
-                    break;
-                case 3:
-                    movimientos();
+                    System.out.println("Saliendo.");
                     break;
             }
-            opcion = Number.askForNumberInt("Selecciona las siguientes opciones: "
-                    + "<0 salir> <1 ingresar> <2 retirar> <3 movimientos>: ", 0, 3);
+        } else {
+            pin = PIN.pedirPIN();
+            while (existe && pin != -1) {
+                existeCuenta = cuentaExiste(dni);
+                if (!existeCuenta) {
+                    crearCuenta(dni);
+                } else {
+                    menu();
+                }
+            }
         }
-        System.out.println("Cerraste sesion.");
     }
 
-    public void ingresar() throws IOException {
+    public void menu() {
+        try {
+
+            int opcion = Number.askForNumberInt("Selecciona las siguientes opciones: "
+                    + "<0 salir> <1 ingresar> <2 retirar> <3 movimientos>: ", 0, 3);
+            while (opcion != 0) {
+                switch (opcion) {
+                    case 1:
+                        ingresar();
+                        break;
+                    case 2:
+                        retirar();
+                        break;
+                    case 3:
+                        movimientos();
+                        break;
+                }
+                opcion = Number.askForNumberInt("Selecciona las siguientes opciones: "
+                        + "<0 salir> <1 ingresar> <2 retirar> <3 movimientos>: ", 0, 3);
+            }
+            System.out.println("Cerraste sesion.");
+        } catch (NumberFormatException ex) {
+        }
+
+    }
+
+    public void ingresar() {
         float importe;
-        importe = Number.askForNumberInt("Que importe desea ingresar?:", 10, 5000);
-        String istCuentas = "INSERT INTO cuentas (balance) "
+        importe = Number.askForNumberFloat("Que importe desea ingresar?: ", 10, 5000);
+        String istCuenta = "INSERT INTO cuenta (balance_cuenta) "
                 + "VALUES ("
                 + "'" + importe + "'"
                 + ")";
         try {
-            st.executeUpdate(istCuentas);
-            System.out.println("Cliente registrado satisfactoriamente.");
-        } catch (SQLException e) {
-            System.err.println(e);
+            st.executeUpdate(istCuenta);
+            System.out.println("Ingreso realizado satisfactoriamente.");
+        } catch (NumberFormatException | SQLException | NullPointerException ex) {
         }
     }
 
     public void retirar() {
         float retiro;
+        retiro = Number.askForNumberFloat("Que importe desea retirar?: ", 10, 500);
+        String istCuenta = "INSERT INTO cuenta (balance_cuenta) "
+                + "VALUES ("
+                + "'" + retiro + "'"
+                + ")";
     }
 
     public void movimientos() {
+
+    }
+
+    public void mostrarBalance() {
 
     }
 
@@ -104,13 +127,13 @@ public class Cajero {
         BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
         String cadena = "";
         try {
-            System.out.println(mensaje);
+            System.out.print(mensaje);
             cadena = teclado.readLine();
             while (cadena.equals("")) {
                 System.out.println("Campo obligatorio.");
                 cadena = teclado.readLine();
             }
-        } catch (IOException es) {
+        } catch (IOException ex) {
         }
         return cadena;
     }
@@ -131,13 +154,13 @@ public class Cajero {
                 String fechaAlta = rs.getString("fecha_alta");
                 clientes.add(new Cliente(dni, nombre, fechaAlta));
             }
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
     }
 
     private void movimientosToArrayList() {
         try {
-            String query = "SELECT * FROM movimientos";
+            String query = "SELECT * FROM movimiento";
             st = acceso.getSt();
             rs = st.executeQuery(query);
             while (rs.next()) {
@@ -148,23 +171,23 @@ public class Cajero {
                 float importeMovimiento = rs.getFloat("importe_movimiento");
                 movimientos.add(new Movimiento(idMovimiento, id_cuenta_fk, fechaAlta, tipoMovimiento, importeMovimiento));
             }
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
     }
 
     private void cuentasToArrayList() {
         try {
-            String query = "SELECT * FROM cuentas";
+            String query = "SELECT * FROM cuenta";
             st = acceso.getSt();
             rs = st.executeQuery(query);
             while (rs.next()) {
                 int idCuenta = rs.getInt("id_cuenta");
                 int id_cliente_fk = rs.getInt("id_cliente_fk");
-                float balance = rs.getFloat("balance");
-                String fechaApertura = rs.getString("fecha_apertura");
+                float balance = rs.getFloat("balance_cuenta");
+                String fechaApertura = rs.getString("fecha_apertura_cuenta");
                 cuentas.add(new Cuenta(idCuenta, id_cliente_fk, balance, fechaApertura));
             }
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
     }
 
@@ -172,19 +195,18 @@ public class Cajero {
         String dniFound;
         boolean result = false;
         try {
-            String query = "SELECT dni_cliente FROM clientes "
+            String query = "SELECT dni_cliente FROM cliente "
                     + "WHERE "
                     + "dni_cliente=" + "'" + dni + "'";
             st = acceso.getSt();
             rs = st.executeQuery(query);
             while (rs.next()) {
-                dni = rs.getString("dni_cliente");
-                dniFound = dni;
-                if (dniFound.compareTo(dni) != 0) {
+                dniFound = rs.getString("dni_cliente");
+                if (dni.equals(dniFound)) {
                     result = true;
                 }
             }
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
         return result;
     }
@@ -205,16 +227,18 @@ public class Cajero {
                     result = true;
                 }
             }
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
         return result;
     }
 
-    public void registrarCliente(String dni, int pin) {
+    public void registrarCliente(String dni) {
         Date fAlta = new Date();
         String nombre;
+        System.out.println("Registrando cliente.");
         nombre = pedirString("Introduzca su nombre: ");
-        String istClientes = "INSERT INTO clientes (dni_cliente,nombre,pin_cliente,fecha_alta) "
+        int pin = PIN.pedirPIN();
+        String istClientes = "INSERT INTO cliente (dni_cliente,nombre_cliente,pin_cliente,fecha_alta_cliente) "
                 + "VALUES ("
                 + "'" + dni + "'"
                 + ","
@@ -227,26 +251,25 @@ public class Cajero {
         try {
             st.executeUpdate(istClientes);
             System.out.println("Cliente registrado satisfactoriamente.");
-        } catch (SQLException | NullPointerException a) {
+        } catch (SQLException | NullPointerException ex) {
         }
     }
 
     public void crearCuenta(String dni) {
         Date fApertura = new Date();
-        float balance = 0.00f;
-        String istCuentas = "INSERT INTO clientes (id_cuenta,id_cliente_fk,balance,fecha_apertura) "
+        float balance = 0f;
+        String istCuentas = "INSERT INTO cuenta (id_cliente_fk,balance_cuenta,fecha_apertura_cuenta) "
                 + "VALUES ("
                 + "'" + dni + "'"
                 + ","
                 + "'" + balance + "'"
                 + ","
-                + "'" + fApertura + "'"
+                + "'" + fApertura.completeDate() + "'"
                 + ")";
         try {
             st.executeUpdate(istCuentas);
-            System.out.println("Cliente registrado satisfactoriamente.");
-        } catch (SQLException e) {
-            System.err.println(e);
+            System.out.println("Cuenta creada satisfactoriamente.");
+        } catch (SQLException | NullPointerException ex) {
         }
     }
 }
